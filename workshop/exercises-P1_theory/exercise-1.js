0c44e3bbbc833951e2aa29c0a44e9b037fa53c33
johnnytortorici <johnny.tortorici@gmail.com>
2020-06-22 13:21:29 -0400

    Exercise 1-1 to 1-4

--- workshop/exercises-P1_theory/exercise-1.js
+++ workshop/exercises-P1_theory/exercise-1.js
@@ -21,12 +21,25 @@ const reverseString = (str) => {
 // Expand each of the following and get the result of the expression
 // 1-1
 square(decrement(square(decrement(3))));
+// First, decrement(3) will be called and return 2
+// Second, square(2) will be called and return 4
+// Third, decrement(4) will be called and return 3
+// Last, square(3) will be called and return 9
 
 // 1-2
 decrement(decrement(square(square(3))));
+// First, square(3) will be called and return 9
+// Second, square(9) will be called and return 81
+// Third, decrement(81) will be called and return 80
+// Last, decrement(80) will be called and return 79
 
 // 1-3
 duplicateString(reverseString('hello'));
+// First, reverseString('hello') will be called and return 'olleh'
+// Second, duplicateString('olleh') will be called and return 'olleholleh'
 
 // 1-4
 reverseString(duplicateString(duplicateString('foo')));
+// First, duplicateString('foo') will be called and return 'foofoo'
+// Second, duplicateString('foofoo') will be called and return 'foofoofoofoo'
+// Last, reverseString('foofoofoofoo') will be called and return 'oofoofoofoof'
